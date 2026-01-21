ff68f959325b8cd88799560e97df4ec77ff70a40
Saber <saber.pourrahimi.1999@gmail.com>
2026-01-21 23:34:11 +0100

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,37 +1,53 @@
 import { createInterface } from "readline"
+import { existsSync, accessSync, constants } from "fs"
 
 const rl = createInterface({
   input: process.stdin,
   output: process.stdout,
 })
 
-rl.setPrompt("$ ")
-rl.prompt()
+const builtinCommands = ["echo", "exit", "type"]
+const paths = process.env["PATH"]?.split(":") || []
 
-const builtInCommands = ["exit", "echo", "type"]
+const echo = (input: string): void => {
+  rl.write(`${input}\n`)
+}
 
-rl.on("line", (line) => {
-  const command = line.trim()
-  const commandName = command.split(" ")[0]
-  switch (commandName) {
-    case "exit":
-      rl.close()
-      return
-    case "echo":
-      console.log(command.slice(5))
-      break
-    case "type":
-      builtInCommands.includes(command.split(" ")[1])
-        ? console.log(`${command.split(" ")[1]} is a shell builtin`)
-        : console.log(`${command.slice(5)}: not found`)
-      break
-    default:
-      console.log(`${command}: command not found`)
-      break
+const type = (input: string): void => {
+  if (builtinCommands.includes(input)) {
+    rl.write(`${input} is a shell builtin\n`)
+  } else {
+    for (const p of paths) {
+      const filePath = `${p}/${input}`
+      if (existsSync(filePath)) {
+        try {
+          accessSync(filePath, constants.X_OK)
+          rl.write(`${input} is ${filePath}\n`)
+          return
+        } catch (e) {
+          // File exists but not executable, continue searching
+        }
+      }
+    }
+
+    rl.write(`${input}: not found\n`)
   }
-  rl.prompt()
-})
+}
 
-rl.on("close", () => {
-  process.exit(0)
-})
+const main = (): void => {
+  rl.question("$ ", (answer) => {
+    if (answer === "exit") {
+      process.exit(0)
+    } else if (answer.startsWith("echo")) {
+      echo(answer.slice(5))
+    } else if (answer.startsWith("type")) {
+      type(answer.slice(5))
+    } else {
+      rl.write(`${answer}: command not found\n`)
+    }
+
+    main()
+  })
+}
+
+main()
