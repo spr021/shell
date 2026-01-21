591a365ac836a504ebba109db7b400ac4842b4f2
Saber <saber.pourrahimi.1999@gmail.com>
2026-01-21 22:40:05 +0100

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,29 +1,37 @@
-import { createInterface } from "readline";
+import { createInterface } from "readline"
 
 const rl = createInterface({
   input: process.stdin,
   output: process.stdout,
-});
+})
 
-rl.setPrompt("$ ");
-rl.prompt();
+rl.setPrompt("$ ")
+rl.prompt()
+
+const builtInCommands = ["exit", "echo", "type"]
 
 rl.on("line", (line) => {
-  const command = line.trim();
-  switch (command) {
+  const command = line.trim()
+  const commandName = command.split(" ")[0]
+  switch (commandName) {
     case "exit":
-      rl.close();
-      return;
-    case command.split(" ")[0] === "echo" ? command : "":
-      console.log(command.slice(5));
-      break;
+      rl.close()
+      return
+    case "echo":
+      console.log(command.slice(5))
+      break
+    case "type":
+      builtInCommands.includes(command.split(" ")[1])
+        ? console.log(`${command.split(" ")[1]} is a shell builtin`)
+        : console.log(`${command.slice(5)}: not found`)
+      break
     default:
-      console.log(`${command}: command not found`);
-      break;
+      console.log(`${command}: command not found`)
+      break
   }
-  rl.prompt();
-});
+  rl.prompt()
+})
 
 rl.on("close", () => {
-  process.exit(0);
-});
+  process.exit(0)
+})
